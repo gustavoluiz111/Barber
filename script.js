f5a21ec3800f2d3eee8605b8a26e31ad1bc2caba
gustavoluiz111 <gustavoluizz14102008@gmail.com>
2025-08-28 15:42:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Animação de scroll fade-in
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+faders.forEach(fader => {
+  observer.observe(fader);
+});
